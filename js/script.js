65d5f26bd6b14afa0b7dbc36713423f716df991f
Merey <88mb@mail.ru>
2022-07-04 16:57:04 +0600

    add video module

--- js/script.js
+++ js/script.js
@@ -6,3 +6,18 @@ btn.addEventListener("click", () => {
     nav.classList.toggle("flex");
     nav.classList.toggle("hidden");
 })
+
+// video modal
+var videoBtn            = document.getElementById("video-btn");
+var videoModal          = document.getElementById("video-modal");
+var videoModalClose     = document.getElementById("video-close-button");
+
+videoBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    videoModal.classList.toggle("hidden");
+})
+
+videoModalClose.addEventListener("click", (event) => {
+    event.preventDefault();
+    videoModal.classList.toggle("hidden");
+})
