f612e9d5e16c5344cceea10fbecfbfc927fa0929
Merey <88mb@mail.ru>
2022-07-05 15:18:50 +0600

    add youtube video frame optimization

--- js/script.js
+++ js/script.js
@@ -15,9 +15,52 @@ var videoModalClose     = document.getElementById("video-close-button");
 videoBtn.addEventListener("click", (event) => {
     event.preventDefault();
     videoModal.classList.toggle("hidden");
+    youtubeOptimize();
 })
 
 videoModalClose.addEventListener("click", (event) => {
     event.preventDefault();
     videoModal.classList.toggle("hidden");
+    youtubeStopVideo();
 })
+
+// youtube frame optimization
+
+function youtubeOptimize() {
+
+	var youtube = document.querySelectorAll( ".youtube" );
+	
+	for (var i = 0; i < youtube.length; i++) {
+		
+		var source = "https://img.youtube.com/vi/"+ youtube[i].dataset.embed +"/sddefault.jpg";
+		
+		var image = new Image();
+				image.src = source;
+				image.addEventListener( "load", function() {
+					youtube[ i ].appendChild( image );
+				}( i ) );
+		
+				youtube[i].addEventListener( "click", function() {
+
+					var iframe = document.createElement( "iframe" );
+
+                            iframe.setAttribute( 'id', 'yt-video-player')
+							iframe.setAttribute( "frameborder", "0" );
+							iframe.setAttribute( "allowfullscreen", "" );
+							iframe.setAttribute( "src", "https://www.youtube.com/embed/"+ this.dataset.embed +"?rel=0&showinfo=0&autoplay=1" );
+
+							this.innerHTML = "";
+							this.appendChild( iframe );
+				} );	
+	};
+	
+};
+
+function youtubeStopVideo () {
+
+    var video = document.getElementById('yt-video-player');
+
+    if (video) {
+        video.parentNode.removeChild(video);
+    }
+};
